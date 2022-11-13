03585462693ee87c00f18dcc4f7c58be42f7a7d6
HimanshuVig <vighimanshuvig@gmail.com>
2022-11-13 21:04:53 +0530

    Glowing BULB Project

---
+++ ind.js
@@ -0,0 +1,8 @@
+function imagechange(){
+    document.getElementById("myImage").src="h1.jpg";
+    document.getElementById("myImage").style.width="250px";
+}
+function imagechange1(){
+    document.getElementById("myImage").src="offBulbImage.jpg";
+    document.getElementById("myImage").style.width="300px";
+}
